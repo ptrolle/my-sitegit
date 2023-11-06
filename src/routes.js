f655661c86351d4248eae08657c28f697d8c93ac
Pablo Trolle <pablotrolle@yahoo.com>
2023-11-06 19:42:24 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function RoutesApp(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default RoutesApp;
